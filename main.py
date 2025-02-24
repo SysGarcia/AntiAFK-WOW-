4c5baa05ba4f6384996fe3371b5e105fa673d8a2
Wolfuliam <isaacbvgf88@gmail.com>
2025-02-24 02:33:33 +0100

    major changes, addition of counters and a lot more

--- main.py
+++ main.py
@@ -1,41 +1,52 @@
 import tkinter as tk
 from tkinter import ttk
 from PIL import ImageTk, Image
-import os
-import utils
+import utils as Utils
 
 master = tk.Tk()
 master.geometry('250x120+0+0')
 master.overrideredirect(True)
 master.call('wm', 'attributes', '.', '-topmost', '1')
 
+secondary = tk.Tk()
+secondary.geometry('550x120+0+120')
+secondary.overrideredirect(True)
+secondary.call('wm', 'attributes', '.', '-topmost', '1')
+
+terciary = tk.Tk()
+terciary.geometry('550x120+0+240')
+terciary.overrideredirect(True)
+terciary.call('wm', 'attributes', '.', '-topmost', '1')
+
+# Establecer el fondo de la ventana secundaria al color que se hará transparente
+secondary.config(bg="#000000")
+secondary.attributes("-transparentcolor", "#000000")
+
+terciary.config(bg="#000000")
+terciary.attributes("-transparentcolor", "#000000")
+
 # Contenedor principal
-main_frame = tk.Frame(master,bg="#f0f0f0",highlightbackground="#3d3d3d", highlightthickness=1)
+main_frame = tk.Frame(master, bg="#f0f0f0", highlightbackground="#3d3d3d", highlightthickness=1)
 main_frame.pack(fill="both", expand=True)
 
 # Frame superior dividido en dos
-top_frame = tk.Frame(main_frame,bg="#f0f0f0")
+top_frame = tk.Frame(main_frame, bg="#f0f0f0")
 top_frame.pack(fill="both", expand=True, side="top")
 
 # Frame izquierdo (mitad de la ventana)
 left_frame = tk.Frame(top_frame, bg="#f0f0f0")
 left_frame.pack(fill="both", expand=True, side="left")
 
-Title = tk.Label(left_frame, text ="Buff catcher",font=("Helvetica", 16),anchor='w', bg="#f0f0f0", fg="#0a0a0a").pack(fill='both')
-Description = tk.Label(left_frame, text ="A wow antiafk tool",font=("Helvetica", 12), bg="#f0f0f0", fg="#0a0a0a",anchor='w').pack(fill='both')
+tk.Label(left_frame, text="Buff catcher", font=("Helvetica", 16), anchor='w', bg="#f0f0f0", fg="#0a0a0a").pack(fill='both')
+tk.Label(left_frame, text="A wow antiafk tool", font=("Helvetica", 12), bg="#f0f0f0", fg="#0a0a0a", anchor='w').pack(fill='both')
 
 # Frame derecho (mitad de la ventana)
 right_frame = tk.Frame(top_frame, bg="#f0f0f0")
 right_frame.pack(fill="both", expand=True, side="right")
 
-Exit = tk.Button(right_frame, text ="❌",font=("Helvetica", 9), command = utils.CloseProgram,anchor="n",width=5, 
-                      border=0, 
-                      background="#d63155", 
-                      activebackground="#ba2949",
-                      highlightthickness=4, 
-                      highlightcolor="#37d3ff", 
-                      highlightbackground="#37d3ff", 
-                      borderwidth=1).pack(side="right",anchor="ne")
+tk.Button(right_frame, text="❌", font=("Helvetica", 9), command=Utils.CloseProgram, anchor="n", width=5,
+          border=0, background="#d63155", activebackground="#ba2949", highlightthickness=4,
+          highlightcolor="#37d3ff", highlightbackground="#37d3ff", borderwidth=1).pack(side="right", anchor="ne")
 
 bottom_frame = tk.Frame(main_frame, bg="#f0f0f0")
 bottom_frame.pack(side="bottom", fill="x")
@@ -44,16 +55,29 @@ s = ttk.Style()
 s.configure('TSeparator', background='#0d0d0d')
 
 border = ttk.Separator(bottom_frame, orient="horizontal")
-border.pack(fill="x", pady=2, padx=2)  
+border.pack(fill="x", pady=2, padx=2)
 
 img = ImageTk.PhotoImage(Image.open(r"Assets/Ony.png"))
-panel = tk.Label(bottom_frame, image=img,bg="#f0f0f0")
-panel.pack(side=tk.LEFT, padx=5)
+tk.Label(bottom_frame, image=img, bg="#f0f0f0").pack(side=tk.LEFT, padx=5)
 
-Start = tk.Button(bottom_frame, bg="#ededed", text="Start", font=("Helvetica", 12), command=lambda: utils.start_action(Start,Stop),activebackground="#235e19")
-Stop = tk.Button(bottom_frame, bg="#ededed", text="Stop", font=("Helvetica", 12), command=lambda: utils.stop_action(Start,Stop), activebackground="#871627")
+Start = tk.Button(bottom_frame, bg="#ededed", text="Start", font=("Helvetica", 12),
+                  command=lambda: Utils.start_action(Start, Stop, secondary_frame, terciary_frame, canvas_secondary,canvas_terciary), activebackground="#235e19")
+Stop = tk.Button(bottom_frame, bg="#ededed", text="Stop", font=("Helvetica", 12),
+                 command=lambda: Utils.stop_action(Start, secondary_frame, terciary_frame, Stop), activebackground="#871627")
 
-Start.pack(side=tk.LEFT, padx=5, pady=10,fill="both", expand=True)
-Stop.pack(side=tk.LEFT, padx=5, pady=10,fill="both", expand=True)
+Start.pack(side=tk.LEFT, padx=5, pady=10, fill="both", expand=True)
+Stop.pack(side=tk.LEFT, padx=5, pady=10, fill="both", expand=True)
+
+canvas_secondary = tk.Canvas(secondary, bg="#000000", highlightbackground="#000000")
+
+secondary_frame = tk.Frame(canvas_secondary, bg="#000000")
+canvas_secondary.pack(side="left", fill="both", expand=True)
+canvas_secondary.create_window((0, 0), window=secondary_frame, anchor="nw")
+
+canvas_terciary = tk.Canvas(terciary, bg="#000000", highlightbackground="#000000")
+
+terciary_frame = tk.Frame(canvas_terciary, bg="#000000")
+canvas_terciary.pack(side="left", fill="both", expand=True)
+canvas_terciary.create_window((0, 0), window=terciary_frame, anchor="nw")
 
 tk.mainloop()
--- utils.py
+++ utils.py
@@ -1,64 +1,125 @@
 import random
 import pyautogui
 import threading
-import time 
+import time
 from win32api import GetSystemMetrics
 import psutil
 import os
+import tkinter as tk
+import datetime
 
 stop_event = threading.Event()
 process_name = "WowClassic.exe"
+first_time = True
 
 def CloseProgram():
-  exit()
+    global stop_event
+    stop_event.set()
+    os._exit(0)  # Salida segura del programa
 
 def random_number():
-    return random.uniform(0, 1)
+    return random.uniform(0.5, 2)  # Evita valores extremadamente pequeños
 
-def keypress_action():
-    while not stop_event.is_set():  # Mientras el evento no esté activado
-      time.sleep(random_number())
-      if random.randint(0, 1) == 1:
-        key = "a"
-      else:
-        key = "d"
-      pyautogui.press(key)
-      print(key)
-
-def search_image():
-  while not stop_event.is_set():
-    time.sleep(30)
-    try:
-      region = (GetSystemMetrics(0) // 2, 0, GetSystemMetrics(0) // 2, GetSystemMetrics(1))  # Mitad derecha
-      location = pyautogui.locateCenterOnScreen(r"Assets/Ony.png", grayscale=True, confidence=0.9, region=region)
-
-      if location:
-        print("Si que la hay en:", location)
-        for proc in psutil.process_iter():
-            if proc.name() == process_name:
-                p = psutil.Process(proc.pid)
-                p.terminate()
-                os.system("shutdown.exe /h")
-                CloseProgram()
-
-        stop_event.set()
-    except Exception as e:
-        print("No hay imagen en la pantalla. Error:", e)
-
-def start_action(start_button, stop_button):
-    global thread
-    stop_event.clear()  # Asegura que el evento está en estado "no detenido"
+def keypress_action(secondary_frame, canvas, label=""):
+    global stop_event
+    if stop_event.is_set():
+        return  # Si el evento está activado, detener la función
+    if label and label != "":
+        label.destroy()
+        time.sleep(0.5)  # Espera antes de crear el nuevo label
     
+    key = "a" if random.randint(0, 1) == 1 else "d"
+    pyautogui.press(key)
+
+    # Actualizar el label en el hilo principal
+    tiempo_actual = time.strftime("%H:%M:%S", time.localtime())
+    new_label = tk.Label(secondary_frame, text=f"{tiempo_actual} {key}", bg="#000000", fg="#faeb14", font=("Helvetica", 13))
+    new_label.pack(fill="both", expand=True)
+
+    # Actualizar la barra de desplazamiento
+    canvas.update_idletasks()
+    canvas.config(scrollregion=canvas.bbox("all"))
+    canvas.yview_moveto(1)  # Mueve el scroll al final
+
+    # Volver a ejecutar después de un tiempo aleatorio en el hilo principal
+    secondary_frame.after(int(random_number() * 1000), lambda: keypress_action(secondary_frame, canvas))
+    
+def search_image(terciary_frame, canvas_terciary, label2=""):
+  global stop_event
+  if stop_event.is_set():
+    return  # Si el evento está activado, detener la función
+
+  if label2 and label2 != "":
+    label2.destroy()
+    time.sleep(0.5)  # Espera antes de crear el nuevo label
+  
+  tiempo_actual = time.strftime("%H:%M:%S", time.localtime())
+
+  try:
+    region = (GetSystemMetrics(0) // 2, 0, GetSystemMetrics(0) // 2, GetSystemMetrics(1))  # Mitad derecha
+    location = pyautogui.locateCenterOnScreen(r"Assets/Ony.png", grayscale=True, confidence=0.9, region=region)
+    if location:
+      new_label = tk.Label(terciary_frame, text=f"{tiempo_actual} Buffo Onyxia encontrado!", bg="#000000", fg="#13dde8", font=("Helvetica", 13))
+      new_label.pack(fill="both", expand=True)
+      
+      canvas_terciary.update_idletasks()
+      canvas_terciary.config(scrollregion=canvas_terciary.bbox("all"))
+      canvas_terciary.yview_moveto(1)  # Mueve el scroll al final
+
+      for proc in psutil.process_iter():
+        if proc.name() == process_name:
+          p = psutil.Process(proc.pid)
+          p.terminate()
+          CloseProgram()
+
+      stop_event.set()
+  except Exception as e:   
+    tiempo_actual_separado = tiempo_actual.split(":")
+    tiempo = datetime.datetime(100, 1, 1, int(tiempo_actual_separado[0]), int(tiempo_actual_separado[1]), int(tiempo_actual_separado[2]))
+
+    # Sumar 30 segundos
+    tiempo_siguiente_busqueda = tiempo + datetime.timedelta(seconds=30)
+
+    # Formatear hora, minuto y segundo con ceros cuando sea necesario
+    hora_formateada = str(tiempo_siguiente_busqueda.hour).zfill(2)
+    minuto_formateado = str(tiempo_siguiente_busqueda.minute).zfill(2)
+    segundo_formateado = str(tiempo_siguiente_busqueda.second).zfill(2)
+
+    new_label = tk.Label(terciary_frame, text=f"{tiempo_actual} Buffo Onyxia no encontrado!", bg="#000000", fg="#de1f45", font=("Helvetica", 13))
+    new_label.pack(side=tk.TOP, anchor=tk.NW)
+    new_label = tk.Label(terciary_frame, text=f"Siguiente busqueda a las {hora_formateada}:{minuto_formateado}:{segundo_formateado}", bg="#000000", fg="#13dde8", font=("Helvetica", 13))
+    new_label.pack(side=tk.TOP, anchor=tk.NW)
+    
+    # Actualizar la barra de desplazamiento
+    canvas_terciary.update_idletasks()
+    canvas_terciary.config(scrollregion=canvas_terciary.bbox("all"))
+    canvas_terciary.yview_moveto(1)  # Mueve el scroll al final
+    
+    terciary_frame.after(30000, lambda: search_image(terciary_frame, canvas_terciary))
+        
+def start_action(start_button, stop_button, secondary_frame, terciary_frame, canvas, canvas_terciary):
+    global stop_event
+    stop_event.clear()
+
     start_button.config(bg="#338525", activebackground="#235e19")
     stop_button.config(bg="#a81b30", activebackground="#871627")
-    
-    thread = threading.Thread(target=keypress_action, daemon=True)
-    thread_image_recognition = threading.Thread(target=search_image, daemon=True)
-    thread.start()
-    thread_image_recognition.start()
 
-def stop_action(start_button, stop_button):
+    # Crear el Label en el hilo principal
+    label = tk.Label(secondary_frame, text="Esperando acción...", bg="#000000", fg="#faeb14",font=("Helvetica", 13))
+    label.pack()
+
+    label2 = tk.Label(terciary_frame, text="Esperando captura...", bg="#000000", fg="#faeb14",font=("Helvetica", 13))
+    label2.pack()
+
+    label.after(1000, lambda: keypress_action(secondary_frame, canvas, label))
+    label2.after(1000, lambda: search_image(terciary_frame, canvas_terciary, label2))
+
+def stop_action(start_button, secondary_frame,terciary_frame,stop_button):
+    global stop_event
     stop_event.set()  # Detiene el bucle en keypress_action
-
+    for widget in secondary_frame.winfo_children():
+        widget.destroy()
+    for widget in terciary_frame.winfo_children():
+        widget.destroy()
     start_button.config(bg="#ededed")
     stop_button.config(bg="#ededed")
